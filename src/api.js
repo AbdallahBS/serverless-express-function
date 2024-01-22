2568606f977804884dbaa566cff4b54cffacd07c
Abdallah Ben Salem <90615628+AbdallahBS@users.noreply.github.com>
2024-01-22 12:44:16 +0100

    changes new

--- src/api.js
+++ src/api.js
@@ -6,27 +6,10 @@ const { Client } = require('pg');
 
 const app = express();
 app.use(cors());
-const client = new Client({
-  connectionString: "postgresql://abdallah:lmv1Px24z_r8Mu4Sa7L6sA@crab-forager-8531.7tc.aws-eu-central-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full",
-  ssl: {
-    rejectUnauthorized: false, 
-  },
-});
+
 
 app.use(express.json()); // For JSON data
 
-// Handle the database connection when http request comming
-app.use(async (req, res, next) => {
-  try {
-    console.log("Connecting to the database...");
-    await client.connect();
-    console.log("Connected to the database.");
-    next();
-  } catch (err) {
-    console.error("Error connecting to the database:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
 
 const router = express.Router();
 const { Send, getMail,createTable } = require('../controller/appController.js');
